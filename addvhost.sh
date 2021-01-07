7c069d5bc0aad06508d0745598a3dfc01a80e8c4
cla <claudio@claudioruiz.com>
2021-01-07 18:33:26 -0300

    Update addvhost.sh

--- addvhost.sh
+++ addvhost.sh
@@ -1,4 +1,61 @@
-# This script should work flawlessly in Ubuntu
+#!/usr/bin/env bash
+#
+# Nginx - new server block
+# http://rosehosting.com
+read -p "Enter domain name : " domain
+read -p "Enter username : " username
+
+# Functions
+ok() { echo -e '\e[32m'$domain'\e[m'; } # Green
+die() { echo -e '\e[1;31m'$domain'\e[m'; exit 1; }
+
+# Variables
+NGINX_AVAILABLE_VHOSTS='/etc/nginx/sites-available'
+NGINX_ENABLED_VHOSTS='/etc/nginx/sites-enabled'
+WEB_DIR='/var/www/$domain'
+WEB_USER=$username
+
+# Sanity check
+[ $(id -g) != "0" ] && die "Script must be run as root."
+#[ $# != "1" ] && die "Usage: $(basename $0) domainName"
+
+# Create nginx config file
+cat > $NGINX_AVAILABLE_VHOSTS/$domain.conf <<EOF
+### www to non-www
+#server {
+#    listen  80;
+#    server_name  www.$domain;
+#    return  301 http://$domain\$request_uri;
+#}
+
+server {
+    listen   80;
+    server_name $domain www.$domain;
+    root  /var/www/$domain;
+    charset  utf-8;
+    index index.php index.html index.htm;
+
+    #access_log $WEB_DIR/logs/$domain-access.log;
+    access_log off;
+
+    error_log $WEB_DIR/logs/$domain-error.log;
+    #error_log off;
+
+
+    ## REWRITES BELOW ##
+    
+    ## INCLUDE COMMONS ##
+
+    include php.conf;
+    include errors.conf;
+    include drop.conf;
+    include expires.conf;
+}
+EOF
+
+# Creating {public,log} directories
+mkdir -p $WEB_DIR/$username/{public_html,logs}
+
 # Create nginx config file
 cat > $NGINX_AVAILABLE_VHOSTS/$domain.conf <<EOF
 ### www to non-www
